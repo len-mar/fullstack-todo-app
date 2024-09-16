30559d32e1fba358c6cee60cf0ada5f8175a437a
marleneanton <the.marlene.anton@gmail.com>
2024-09-16 14:14:50 +0200

    Initial commit

---
+++ src/main/java/org/example/fullstacktodoapp/FullstackTodoAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.fullstacktodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackTodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fullstacktodoapp/FullstackTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fullstacktodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
